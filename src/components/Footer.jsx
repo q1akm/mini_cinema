3eed578c32003618cc1b69aa05bdb45f23bd3623
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-08-05 20:12:41 +0700

    Link changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ import { FaGithubSquare } from "react-icons/fa";
 const Footer = () => 
    (
     <div className='text-white bg-black flex flex-col justify-center items-center gap-2 p-2 '>
-        <h1>Made By akm.web</h1>
+        <h1>Made by <a href="aungkaungmyint.com" className="underline">q1akm</a></h1>
         <div className='flex gap-4 text-lg'>
-        <a href="https://www.instagram.com/akm.web/"><RiInstagramFill/></a>
-        <a href="https://github.com/akmweb"><FaGithubSquare/></a>
+        <a href="https://www.instagram.com/q1akm/"><RiInstagramFill/></a>
+        <a href="https://github.com/q1akm"><FaGithubSquare/></a>
         </div>
     </div>
   )
